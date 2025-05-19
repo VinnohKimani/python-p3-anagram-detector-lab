bd22785294f42017aab104a507f2903164be6f89
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-05-19 23:53:22 +0300

    passed all tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        sorted_original = sorted(self.word)
+        result = []
+
+        for word in word_list:
+            if word.lower() == self.word:
+                continue #skip a word if the word is already in the list
+            if sorted(word.lower()) == sorted_original:
+                result.append(word)
+        return result
